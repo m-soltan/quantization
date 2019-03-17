d28cffae4e6a5d03a6e9fe38530ffb3b92da9e79
Mikolaj Soltan <ms387544@students.mimuw.edu.pl>
2019-03-18 00:24:44 +0100

    implemented the energy structure

--- energy.c
+++ energy.c
@@ -1,10 +1,52 @@
 #include "energy.h"
 
 struct Energy {
-
+	energy_t val;
+	size_t size;
+	Energy *parent;
 };
 
+/*
+ * Auxiliary functions
+ * a Disjoint-Set implementation with path-splitting and union by size
+ */
+
+Energy * find(Energy *x) {
+	for (Energy *next; x->parent != x; x = next) {
+		next = x->parent;
+		x->parent = next->parent;
+	}
+	return x;
+}
+
+Energy * make_set(energy_t val) {
+	Energy *ans = (Energy *) malloc(sizeof(Energy));
+	if (ans) {
+		ans->val = val;
+		ans->parent = ans;
+		ans->size = 1;
+	}
+	return ans;
+}
+
+void set_union(Energy *x, Energy *y) {
+	Energy *xRoot = find(x), *yRoot = find(y);
+	if (xRoot == yRoot)
+		return;
+	if (xRoot->size < yRoot->size) {
+		Energy *temp = xRoot;
+		xRoot = yRoot;
+		yRoot = temp;
+	}
+	yRoot->parent = xRoot;
+	xRoot->size += yRoot->size;
+}
+
+//todo
 void energy_destroy(Energy *x) {
-	//todo
 	(void) x;
+}
+
+void energy_print(const Energy *x) {
+	fprintf(stdout, "%llu\n", x->val);
 }
--- energy.h
+++ energy.h
@@ -1,9 +1,14 @@
-
 #ifndef QUANTIZATION_ENERGY_H
 #define QUANTIZATION_ENERGY_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint64_t energy_t;
 typedef struct Energy Energy;
 
 void energy_destroy(Energy *);
+void energy_print(const Energy *);
 
 #endif //QUANTIZATION_ENERGY_H
