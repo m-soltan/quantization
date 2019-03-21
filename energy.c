5f57a08c7193f0f1b9617388fff0b8be39c45c42
Mikolaj Soltan <ms387544@students.mimuw.edu.pl>
2019-03-21 09:40:46 +0100

    added deletion

--- energy.c
+++ energy.c
@@ -4,9 +4,14 @@ struct Energy {
 	energy_t val;
 	size_t size;
 	Energy *parent;
+	Energy *left, *right;
+	Node *node;
 };
 
 // Auxiliary functions
+
+void energy_attach(Energy *, Energy *);
+void energy_join(Energy *e1, Energy *e2);
 
 energy_t avg(energy_t e1, energy_t e2) {
 	if (e1 > e2) return avg(e2, e1);
@@ -14,9 +19,19 @@ energy_t avg(energy_t e1, energy_t e2) {
 	return e1 + d / 2;
 }
 
+void energy_attach(Energy *e1, Energy *e2) {
+	energy_join(e2->left, e1->right);
+	energy_join(e1, e2);
+}
+
+void energy_join(Energy *e1, Energy *e2) {
+	e1->right = e2;
+	e2->left = e1;
+}
+
 // a Disjoint-Set implementation with path-splitting and union by size
 
-Energy * find(Energy *x) {
+Energy * energy_find(Energy *x) {
 	for (Energy *next; x->parent != x; x = next) {
 		next = x->parent;
 		x->parent = next->parent;
@@ -28,19 +43,19 @@ energy_t energy_convert(uint64_t x) {
 	return x;
 }
 
-Energy *make_set(energy_t val) {
+Energy * energy_init(energy_t val) {
 	Energy *ans = (Energy *) malloc(sizeof(Energy));
 	if (ans) {
 		ans->val = val;
 		ans->parent = ans;
 		ans->size = 1;
+		ans->left = ans->right = ans;
 	}
 	return ans;
 }
 
-
-void set_union(Energy *x, Energy *y) {
-	Energy *x_root = find(x), *y_root = find(y);
+void energy_union(Energy *x, Energy *y) {
+	Energy *x_root = energy_find(x), *y_root = energy_find(y);
 	energy_t root_e = avg(x_root->val, y_root->val);
 	if (x_root == y_root)
 		return;
@@ -52,11 +67,19 @@ void set_union(Energy *x, Energy *y) {
 	y_root->parent = x_root;
 	x_root->size += y_root->size;
 	x_root->val = root_e;
+	energy_attach(x_root, y_root);
 }
 
-//todo
 void energy_destroy(Energy *x) {
-	(void) x;
+	Energy *root = energy_find(x);
+	if (x == root && x != x->left) {
+		Energy *neighbor = x->left;
+		*get_energy(neighbor->node) = x;
+		--x->size;
+		x = neighbor;
+	}
+	energy_join(x->left, x->right);
+	free(x);
 }
 
 void energy_print(const Energy *x) {
--- energy.h
+++ energy.h
@@ -7,11 +7,13 @@
 
 typedef uint64_t energy_t;
 typedef struct Energy Energy;
+typedef struct Node Node;
 
 energy_t energy_convert(uint64_t x);
-Energy * make_set(energy_t);
+Energy * energy_init(energy_t val);
+Energy ** get_energy(Node *x);
 void energy_destroy(Energy *);
 void energy_print(const Energy *);
-void set_union(Energy *x, Energy *y);
+void energy_union(Energy *x, Energy *y);
 
 #endif //QUANTIZATION_ENERGY_H
